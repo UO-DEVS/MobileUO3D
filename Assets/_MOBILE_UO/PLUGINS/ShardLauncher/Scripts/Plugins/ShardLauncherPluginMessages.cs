4e6483d77e6ff57b1a46c2b1a0c8b38ef9bf9cd8
DX4D <DX4Dgames@gmail.com>
2025-04-19 20:34:16 -0700

    Added method name to message plugin

--- Assets/_MOBILE_UO/PLUGINS/ShardLauncher/Scripts/Plugins/ShardLauncherPluginMessages.cs
+++ Assets/_MOBILE_UO/PLUGINS/ShardLauncher/Scripts/Plugins/ShardLauncherPluginMessages.cs
@@ -15,40 +15,40 @@ public class ShardLauncherPluginMessages : LauncherPlugin
 	
 	public override void OnLoad(ShardConfiguration shard)
 	{
-		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay);
+		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnLoad");
 	}
 	
 	//ON SERVER CHANGED
 	public override void OnServerChanged(ShardConfiguration oldServer, ShardConfiguration newServer)
 	{
-		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay);
+		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnServerChanged");
 	}
 	
 	//ON SERVER LAUNCHING
 	public override void OnBeforeServerLaunch(ShardConfiguration shard)
 	{
-		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay);
+		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnBeforeServerLaunch");
 	}
 	//ON SERVER LAUNCHED
 	public override void OnServerLaunch(ShardConfiguration shard)
 	{
-		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay);
+		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnServerLaunch");
 	}
 	//ON GAME CLIENT START
 	public override void OnGameClientStart()
 	{
-		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay);
+		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnGameClientStart");
 	}
 	
 	//ON CLIENT DOWNLOAD START
 	public override void OnClientDownloadStart()
 	{
-		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay);
+		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnClientDownloadStart");
 	}
     
 	//ON DOWNLOADS DELETED
 	public override void OnDownloadsDeleted(ServerConfiguration server)
 	{
-		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay);
+		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnDownloadsDeleted");
 	}
 }
