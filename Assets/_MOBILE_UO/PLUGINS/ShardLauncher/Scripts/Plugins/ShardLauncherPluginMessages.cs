70d8ae49d4f6653e3f9e1a1d1be967889acfa6a7
DX4D <DX4Dgames@gmail.com>
2025-06-17 12:02:40 -0700

    QoL: Added more control options to the Message Launcher Plugin
    -You can now toggle which triggers the plugin will respond to individually

--- Assets/_MOBILE_UO/PLUGINS/ShardLauncher/Scripts/Plugins/ShardLauncherPluginMessages.cs
+++ Assets/_MOBILE_UO/PLUGINS/ShardLauncher/Scripts/Plugins/ShardLauncherPluginMessages.cs
@@ -1,5 +1,17 @@
 ﻿using UnityEngine;
 
+[System.Serializable]
+public struct LauncherPluginEvents
+{
+	public bool OnLoad;
+	public bool OnServerChanged;
+	public bool OnBeforeServerLaunch;
+	public bool OnServerLaunch;
+	public bool OnGameClientStart;
+	public bool OnClientDownloadStart;
+	public bool OnDownloadsDeleted;
+}
+
 [CreateAssetMenu(fileName = "Messenger Plugin", menuName = "UO/Launcher/Plugin/Messenger Plugin")]
 public class ShardLauncherPluginMessages : LauncherPlugin
 {
@@ -13,42 +25,52 @@ public class ShardLauncherPluginMessages : LauncherPlugin
 		#endif
 	}
 	
+	[SerializeField] LauncherPluginEvents config;
+	
+	//ON LOAD
 	public override void OnLoad(ShardConfiguration shard)
 	{
+		if (!config.OnLoad) return;
 		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnLoad");
 	}
 	
 	//ON SERVER CHANGED
 	public override void OnServerChanged(ShardConfiguration oldServer, ShardConfiguration newServer)
 	{
+		if (!config.OnServerChanged) return;
 		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnServerChanged");
 	}
 	
 	//ON SERVER LAUNCHING
 	public override void OnBeforeServerLaunch(ShardConfiguration shard)
 	{
+		if (!config.OnBeforeServerLaunch) return;
 		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnBeforeServerLaunch");
 	}
 	//ON SERVER LAUNCHED
 	public override void OnServerLaunch(ShardConfiguration shard)
 	{
+		if (!config.OnServerLaunch) return;
 		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnServerLaunch");
 	}
 	//ON GAME CLIENT START
 	public override void OnGameClientStart()
 	{
+		if (!config.OnGameClientStart) return;
 		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnGameClientStart");
 	}
 	
 	//ON CLIENT DOWNLOAD START
 	public override void OnClientDownloadStart()
 	{
+		if (!config.OnClientDownloadStart) return;
 		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnClientDownloadStart");
 	}
     
 	//ON DOWNLOADS DELETED
 	public override void OnDownloadsDeleted(ServerConfiguration server)
 	{
+		if (!config.OnDownloadsDeleted) return;
 		if (!string.IsNullOrWhiteSpace(AlwaysSay)) Say(AlwaysSay + "\n OnDownloadsDeleted");
 	}
 }
