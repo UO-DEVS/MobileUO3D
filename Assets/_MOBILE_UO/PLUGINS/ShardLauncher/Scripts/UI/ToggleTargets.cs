7e6856d26732b6a6df24b9049ae96b7057bd4443
DX4D <DX4Dgames@gmail.com>
2025-06-16 21:14:55 -0700

    UPDATE: Togg;eTargets added functionality

--- Assets/_MOBILE_UO/PLUGINS/ShardLauncher/Scripts/UI/ToggleTargets.cs
+++ Assets/_MOBILE_UO/PLUGINS/ShardLauncher/Scripts/UI/ToggleTargets.cs
@@ -12,6 +12,9 @@ namespace UI.Tools
 		[SerializeField] bool _swap = true;
 		private bool Swap => _swap;
 		
+		[Header("TARGET SETTINGS")]
+		[SerializeField] bool unique = true;
+		
 		[Header("LINKED OBJECTS")]
 		//TARGETS
 		[SerializeField] List<Transform> _targets = new List<Transform>();
@@ -21,7 +24,7 @@ namespace UI.Tools
 		{
 			foreach (Transform target in _targets)
 			{
-				target.gameObject.SetActive(Swap);
+				ToggleOff(target);
 			}
 		}
 		//ON ENABLE
@@ -29,8 +32,33 @@ namespace UI.Tools
 		{
 			foreach (Transform target in _targets)
 			{
-				target.gameObject.SetActive(!Swap);
+				ToggleOn(target);
 			}
+		}
+		//TOGGLE ON
+		public void ToggleOn()
+		{
+			ToggleOn(transform);
+		}
+		public void ToggleOn(Transform target)
+		{
+				target.gameObject.SetActive(!Swap);
+		}
+		//TOGGLE OFF
+		public void ToggleOff()
+		{
+			ToggleOff(transform);
+		}
+		public void ToggleOff(Transform target)
+		{
+			target.gameObject.SetActive(Swap);
+		}
+		
+		public void Add(Transform target)
+		{
+			
+			if (unique && _targets.Contains(target)) return;
+			_targets.Add(target);
 		}
 	}
 }
