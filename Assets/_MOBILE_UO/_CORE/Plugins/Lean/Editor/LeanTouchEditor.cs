939c2fbf80158b00ac59cf80d20c05759feeb9fb
DX4D <DX4Dgames@gmail.com>
2025-06-29 23:58:31 -0700

    FIX: Lean Touch - Separated Editor Logic

---
+++ Assets/_MOBILE_UO/_CORE/Plugins/Lean/Editor/LeanTouchEditor.cs
@@ -0,0 +1,136 @@
+﻿// Some platforms may report incorrect finger ID data, or be too strict with how close a finger must be between taps
+// If you're developing on a platform or device like this, you can uncomment this to enable manual override of the ID.
+//#define LEAN_ALLOW_RECLAIM
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections.Generic;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+#if UNITY_EDITOR
+namespace Lean.Touch
+{
+	[CustomEditor(typeof(LeanTouch))]
+	public class LeanTouchEditor : Editor
+	{
+		private static List<LeanFinger> allFingers = new List<LeanFinger>();
+
+		private static GUIStyle fadingLabel;
+
+		[MenuItem("GameObject/Lean/Touch", false, 1)]
+		public static void CreateTouch()
+		{
+			var gameObject = new GameObject(typeof(LeanTouch).Name);
+
+			Undo.RegisterCreatedObjectUndo(gameObject, "Create Touch");
+
+			gameObject.AddComponent<LeanTouch>();
+
+			Selection.activeGameObject = gameObject;
+		}
+
+		// Draw the whole inspector
+		public override void OnInspectorGUI()
+		{
+			if (LeanTouch.Instances.Count > 1)
+			{
+				EditorGUILayout.HelpBox("There is more than one active and enabled LeanTouch...", MessageType.Warning);
+
+				EditorGUILayout.Separator();
+			}
+
+			var touch = (LeanTouch)target;
+
+			EditorGUILayout.Separator();
+
+			DrawSettings(touch);
+
+			EditorGUILayout.Separator();
+
+			DrawFingers(touch);
+
+			EditorGUILayout.Separator();
+
+			Repaint();
+		}
+
+		private void DrawSettings(LeanTouch touch)
+		{
+			DrawDefault("TapThreshold");
+			DrawDefault("SwipeThreshold");
+#if LEAN_ALLOW_RECLAIM
+			DrawDefault("ReclaimThreshold");
+#endif
+			DrawDefault("ReferenceDpi");
+			DrawDefault("GuiLayers");
+
+			EditorGUILayout.Separator();
+
+			DrawDefault("SimulateMultiFingers");
+
+			if (touch.SimulateMultiFingers == true)
+			{
+				EditorGUI.indentLevel++;
+					DrawDefault("PinchTwistKey");
+					DrawDefault("MovePivotKey");
+					DrawDefault("MultiDragKey");
+					DrawDefault("FingerTexture");
+				EditorGUI.indentLevel--;
+			}
+		}
+
+		private void DrawFingers(LeanTouch touch)
+		{
+			EditorGUILayout.LabelField("Fingers", EditorStyles.boldLabel);
+
+			allFingers.Clear();
+			allFingers.AddRange(LeanTouch.Fingers);
+			allFingers.AddRange(LeanTouch.InactiveFingers);
+			allFingers.Sort((a, b) => a.Index.CompareTo(b.Index));
+
+			for (var i = 0; i < allFingers.Count; i++)
+			{
+				var finger   = allFingers[i];
+				var progress = touch.TapThreshold > 0.0f ? finger.Age / touch.TapThreshold : 0.0f;
+				var style    = GetFadingLabel(finger.Set, progress);
+
+				if (style.normal.textColor.a > 0.0f)
+				{
+					var screenPosition = finger.ScreenPosition;
+
+					EditorGUILayout.LabelField("#" + finger.Index + " x " + finger.TapCount + " (" + Mathf.FloorToInt(screenPosition.x) + ", " + Mathf.FloorToInt(screenPosition.y) + ") - " + finger.Age.ToString("0.0"), style);
+				}
+			}
+		}
+
+		private void DrawDefault(string name)
+		{
+			EditorGUI.BeginChangeCheck();
+
+			EditorGUILayout.PropertyField(serializedObject.FindProperty(name));
+
+			if (EditorGUI.EndChangeCheck() == true)
+			{
+				serializedObject.ApplyModifiedProperties();
+			}
+		}
+
+		private static GUIStyle GetFadingLabel(bool active, float progress)
+		{
+			if (fadingLabel == null)
+			{
+				fadingLabel = new GUIStyle(EditorStyles.label);
+			}
+
+			var a = EditorStyles.label.normal.textColor;
+			var b = a; b.a = active == true ? 0.5f : 0.0f;
+
+			fadingLabel.normal.textColor = Color.Lerp(a, b, progress);
+
+			return fadingLabel;
+		}
+	}
+}
+#endif
--- Assets/_MOBILE_UO/_CORE/Plugins/Lean/LeanTouch.cs
+++ Assets/_MOBILE_UO/_CORE/Plugins/Lean/LeanTouch.cs
@@ -5,9 +5,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 using System.Collections.Generic;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace Lean.Touch
 {
@@ -387,13 +384,13 @@ namespace Lean.Touch
 			// Set the finger texture?
 			if (FingerTexture == null)
 			{
-				var guids = AssetDatabase.FindAssets("FingerVisualization t:texture2d");
+				var guids = UnityEditor.AssetDatabase.FindAssets("FingerVisualization t:texture2d");
 
 				if (guids.Length > 0)
 				{
-					var path = AssetDatabase.GUIDToAssetPath(guids[0]);
+					var path = UnityEditor.AssetDatabase.GUIDToAssetPath(guids[0]);
 
-					FingerTexture = AssetDatabase.LoadMainAssetAtPath(path) as Texture2D;
+					FingerTexture = UnityEditor.AssetDatabase.LoadMainAssetAtPath(path) as Texture2D;
 				}
 			}
 #endif
@@ -755,129 +752,3 @@ namespace Lean.Touch
 		}
 	}
 }
-
-#if UNITY_EDITOR
-namespace Lean.Touch
-{
-	[CustomEditor(typeof(LeanTouch))]
-	public class LeanTouch_Editor : Editor
-	{
-		private static List<LeanFinger> allFingers = new List<LeanFinger>();
-
-		private static GUIStyle fadingLabel;
-
-		[MenuItem("GameObject/Lean/Touch", false, 1)]
-		public static void CreateTouch()
-		{
-			var gameObject = new GameObject(typeof(LeanTouch).Name);
-
-			Undo.RegisterCreatedObjectUndo(gameObject, "Create Touch");
-
-			gameObject.AddComponent<LeanTouch>();
-
-			Selection.activeGameObject = gameObject;
-		}
-
-		// Draw the whole inspector
-		public override void OnInspectorGUI()
-		{
-			if (LeanTouch.Instances.Count > 1)
-			{
-				EditorGUILayout.HelpBox("There is more than one active and enabled LeanTouch...", MessageType.Warning);
-
-				EditorGUILayout.Separator();
-			}
-
-			var touch = (LeanTouch)target;
-
-			EditorGUILayout.Separator();
-
-			DrawSettings(touch);
-
-			EditorGUILayout.Separator();
-
-			DrawFingers(touch);
-
-			EditorGUILayout.Separator();
-
-			Repaint();
-		}
-
-		private void DrawSettings(LeanTouch touch)
-		{
-			DrawDefault("TapThreshold");
-			DrawDefault("SwipeThreshold");
-#if LEAN_ALLOW_RECLAIM
-			DrawDefault("ReclaimThreshold");
-#endif
-			DrawDefault("ReferenceDpi");
-			DrawDefault("GuiLayers");
-
-			EditorGUILayout.Separator();
-
-			DrawDefault("SimulateMultiFingers");
-
-			if (touch.SimulateMultiFingers == true)
-			{
-				EditorGUI.indentLevel++;
-					DrawDefault("PinchTwistKey");
-					DrawDefault("MovePivotKey");
-					DrawDefault("MultiDragKey");
-					DrawDefault("FingerTexture");
-				EditorGUI.indentLevel--;
-			}
-		}
-
-		private void DrawFingers(LeanTouch touch)
-		{
-			EditorGUILayout.LabelField("Fingers", EditorStyles.boldLabel);
-
-			allFingers.Clear();
-			allFingers.AddRange(LeanTouch.Fingers);
-			allFingers.AddRange(LeanTouch.InactiveFingers);
-			allFingers.Sort((a, b) => a.Index.CompareTo(b.Index));
-
-			for (var i = 0; i < allFingers.Count; i++)
-			{
-				var finger   = allFingers[i];
-				var progress = touch.TapThreshold > 0.0f ? finger.Age / touch.TapThreshold : 0.0f;
-				var style    = GetFadingLabel(finger.Set, progress);
-
-				if (style.normal.textColor.a > 0.0f)
-				{
-					var screenPosition = finger.ScreenPosition;
-
-					EditorGUILayout.LabelField("#" + finger.Index + " x " + finger.TapCount + " (" + Mathf.FloorToInt(screenPosition.x) + ", " + Mathf.FloorToInt(screenPosition.y) + ") - " + finger.Age.ToString("0.0"), style);
-				}
-			}
-		}
-
-		private void DrawDefault(string name)
-		{
-			EditorGUI.BeginChangeCheck();
-
-			EditorGUILayout.PropertyField(serializedObject.FindProperty(name));
-
-			if (EditorGUI.EndChangeCheck() == true)
-			{
-				serializedObject.ApplyModifiedProperties();
-			}
-		}
-
-		private static GUIStyle GetFadingLabel(bool active, float progress)
-		{
-			if (fadingLabel == null)
-			{
-				fadingLabel = new GUIStyle(EditorStyles.label);
-			}
-
-			var a = EditorStyles.label.normal.textColor;
-			var b = a; b.a = active == true ? 0.5f : 0.0f;
-
-			fadingLabel.normal.textColor = Color.Lerp(a, b, progress);
-
-			return fadingLabel;
-		}
-	}
-}
-#endif
